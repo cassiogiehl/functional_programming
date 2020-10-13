0cb369b72e959d51ad844f9d7caf5b5cff92e76d
Cássio Giehl da Rosa <cassio.giehl@gmail.com>
2020-10-13 20:28:47 -0300

    fix ex6

--- ex6_array_of_n_elements.scala
+++ ex6_array_of_n_elements.scala
@@ -1,17 +1,11 @@
 /*
-    Create an array of  integers, where the value of  is passed as an 
+    Create an array of N integers, where the value of N is passed as an 
     argument to the pre-filled function in your editor. This challenge 
     uses a custom checker, so you can create any array of  integers. For 
     example, if "n=4", you could return [1, 1, 1, 1], [1, 2, 3, 4], or 
     any other array of equal length.
 */
 
-def f(num: Int): List[Int] = {
-    
-    val list = List.fill(num)(1)
+def f(num: Int) : List[Int] = List.fill(num)(1)
 
-    print(list + "\n")
-    return list
-}
-
-f(10)
+print(f(10))
